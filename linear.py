7791beda48a7f445c5f6f6faea5c68af8ce7bd15
sheza20251112-collab <sheza20251112@miis.ae>
2026-05-06 09:56:52 +0400

    Update linear.py

--- linear.py
+++ linear.py
@@ -13,6 +13,6 @@ st.write("Enter hours studied to predict the exam score.")
 hours = st.number_input("Hours Studied:", min_value = 0.0, step = 0.1)
 if st.button("Predict Score"):
   predicted_score = model.predict([[hours]]) [0]
-  st.success(f"Predicted Score: {predicted_score:.2f}")
+  st.success(f"Predicted Score: {predicted_score: .2f}")
 st.write("### Sample Training Data")
 st.dataframe(df)
